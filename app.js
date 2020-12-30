7a09ffc53c2b0164c752201900272b6ca8958c28
AngryToo <73968731+AngryToo@users.noreply.github.com>
2020-12-30 15:12:04 -0500

    Create app.js

---
+++ app.js
@@ -0,0 +1,10 @@
+{
+  "name": "Angry Too Arras",
+  "description": "A fanmade sequel to diep.io but killer version",
+  "scripts": {
+    "build": "gulp build",
+    "start": "gulp run",
+    "watch": "gulp watch",
+    "test": "gulp test"
+  }
+}
